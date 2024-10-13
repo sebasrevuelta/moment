2f39017e883ff4b73eb5c6ee2980d03181d61483
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2024-10-13 15:39:51 +0200

    Update fA.js

--- packages/serviceA/fA.js
+++ packages/serviceA/fA.js
@@ -2,4 +2,4 @@ const moment = require('moment');
 
 // Get the current date and time
 const now = moment();
-console.log(now.format());  // Output: 2024-06-19T10:00:00+00:00 (example)
+console.log(now.format()); 
